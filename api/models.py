267f12c6304e9f37852cf38da2315a58bf06da5b
audreyhsiao <audreyyj13@gmail.com>
2025-01-23 21:23:08 -0600

    update movie and rating models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=32)
+    description = models.TextField(max_length=360)
+    
+class Rating(models.Model):
+    movie = models.ForeignKey(Movie, on_delete = models.CASCADE) # if we remove the movie, we remove the rating
+    user = models.ForeignKey(User, on_delete = models.CASCADE) # one to one # if we remove the user, we remove the rating
+    stars = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+    class Meta:
+        unique_together = (('user','movie'),) # only accept the rating when (user,movie) is not in the database
+        index_together = (('user','movie'),)
