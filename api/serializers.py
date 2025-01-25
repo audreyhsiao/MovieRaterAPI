2120960ac94016353472bd9c9208229af13267d0
audreyhsiao <audreyyj13@gmail.com>
2025-01-24 22:48:58 -0600

    update movie and rating models

---
+++ api/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Movie, Rating
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie # tell serializer what model we are using
+        fields = ('id','title','description') # tell serializer which part we are using it
+        
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = ('id','stats','user','movie')
--- api/urls.py
+++ api/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
+from .views import MovieViewSet, RatingViewSet
 
 router = routers.DefaultRouter()
+router.register('movies', MovieViewSet)
+router.register('ratings', RatingViewSet)
 
 urlpatterns = [
     path('', include(router.urls)), 
